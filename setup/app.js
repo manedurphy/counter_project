aaf7480a9d55a3f50179baa3da1a63a29a518f7d
Dane Murphy <danem7@gmail.com>
2020-07-05 20:46:23 -0700

    used query selector all to assign value and btns

---
+++ setup/app.js
@@ -0,0 +1,8 @@
+//set up an initial counter value... obviously 0
+let count = 0;
+
+const value = document.querySelectorAll('#value');
+const btns = document.querySelectorAll('.btn');
+
+
+console.log(btns);
