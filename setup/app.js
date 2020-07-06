39c61286bf4fe936e5da02cc78932023d03d23dc
Dane Murphy <danem7@gmail.com>
2020-07-05 21:30:45 -0700

    Color of value changes above and below 0

--- setup/app.js
+++ setup/app.js
@@ -16,6 +16,13 @@ btns.forEach(function (btn) {
     } else {
       count++;
     }
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count === 0) {
+      value.style.color = "blue";
+    } else {
+      value.style.color = "red";
+    }
     value.textContent = count;
   })
 });
