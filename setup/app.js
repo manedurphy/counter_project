feffefc45504a91c955ef9d9196c419912f85a04
Dane Murphy <danem7@gmail.com>
2020-07-05 21:09:50 -0700

    Added functionality to reset, increase, and deacrease buttons

--- setup/app.js
+++ setup/app.js
@@ -1,8 +1,21 @@
 //set up an initial counter value... obviously 0
 let count = 0;
 
-const value = document.querySelectorAll('#value');
+const value = document.querySelector('#value');
 const btns = document.querySelectorAll('.btn');
 
 
-console.log(btns);
+// console.log(btns);
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function (event) {
+    const styles = event.currentTarget.classList;
+    if (styles.contains('decrease')) {
+      count--;
+    } else if (styles.contains('reset')) {
+      count = 0;
+    } else {
+      count++;
+    }
+    value.textContent = count;
+  })
+});
